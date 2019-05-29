54b67f8a541fa138dcb116ce0d0c3de4cab1ec43
jungeunlee95 <leeap1004@gmail.com>
2019-05-29 10:19:47 +0900

    mysite3 - view resolver 명시적 @Bean

--- spring-study-springex3/src/main/java/com/cafe24/springex/controller/HelloController.java
+++ spring-study-springex3/src/main/java/com/cafe24/springex/controller/HelloController.java
@@ -13,7 +13,7 @@ public class HelloController {
 	
 	@RequestMapping("/hello")
 	public String hello() {
-		return "/WEB-INF/views/test/hello.jsp";
+		return "test/hello";
 	}
 	
 	@RequestMapping("/hello2")
